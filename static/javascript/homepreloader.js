25f87db420fae1679d382953e46e8961b94d1727
Pavittar Singh <pavittarsingh12364@gmail.com>
2020-08-09 14:40:12 -0700

    templates and static are in production mode

--- static/javascript/homepreloader.js
+++ static/javascript/homepreloader.js
@@ -1,45 +1,45 @@
-if (document.readyState == 'loading') {
-	document.addEventListener('DOMContentLoaded', ready)
-} else {
-	ready()
-}
-
-function ready() {
-// place the donation request here when some searches a redirect to donates shows up do the thing below
-	document.getElementById('donations').style.display='none';
-	console.log('Hello World')
-}
-
-function homepreloader() {
-    document.getElementById("homepreloaderid").innerHTML =
-    `<div class="row">
-        <div class="col s12 m3 l2"></div>
-        <div class="col s12 m6 l8">
-            <h6 class="center container preloaderheader">Please wait while we gather your results</h6>
-            <h6 class="center container preloaderrightsnotice">
-                All rights of the product belong to the redirected seller.
-            </h6>
-            <div class="prodiv">
-                <div class="proind"></div>
-            </div>
-        </div>
-        <div class="col s12 m3 l2"></div>
-    </div>`;
-
-
-
-    document.querySelector('.mainhomecontent').style.display='none';
-    document.querySelector('.prodiv').classList.add('progress');
-    document.querySelector('.proind').classList.add('indeterminate');
-    document.querySelector('.preloaderheader').style.display='block';
-    document.getElementById('donations').style.display='block';
-    document.querySelector('.preloaderrightsnotice').style.display='block';
-    setTimeout(function(){ document.querySelector('.prodiv').classList.remove('progress');
-    document.querySelector('.proind').classList.remove('indeterminate');
-    document.querySelector('.mainhomecontent').style.display='block';
-    document.querySelector('.preloaderrightsnotice').style.display='none';
-    document.querySelector('.preloaderheader').style.display='none';
-    },100000);
-}
-
-
+//if (document.readyState == 'loading') {
+//	document.addEventListener('DOMContentLoaded', ready)
+//} else {
+//	ready()
+//}
+//
+//function ready() {
+//// place the donation request here when some searches a redirect to donates shows up do the thing below
+//	document.getElementById('donations').style.display='none';
+//	console.log('Hello World')
+//}
+//
+//function homepreloader() {
+//    document.getElementById("homepreloaderid").innerHTML =
+//    `<div class="row">
+//        <div class="col s12 m3 l2"></div>
+//        <div class="col s12 m6 l8">
+//            <h6 class="center container preloaderheader">Please wait while we gather your results</h6>
+//            <h6 class="center container preloaderrightsnotice">
+//                All rights of the product belong to the redirected seller.
+//            </h6>
+//            <div class="prodiv">
+//                <div class="proind"></div>
+//            </div>
+//        </div>
+//        <div class="col s12 m3 l2"></div>
+//    </div>`;
+//
+//
+//
+//    document.querySelector('.mainhomecontent').style.display='none';
+//    document.querySelector('.prodiv').classList.add('progress');
+//    document.querySelector('.proind').classList.add('indeterminate');
+//    document.querySelector('.preloaderheader').style.display='block';
+//    document.getElementById('donations').style.display='block';
+//    document.querySelector('.preloaderrightsnotice').style.display='block';
+//    setTimeout(function(){ document.querySelector('.prodiv').classList.remove('progress');
+//    document.querySelector('.proind').classList.remove('indeterminate');
+//    document.querySelector('.mainhomecontent').style.display='block';
+//    document.querySelector('.preloaderrightsnotice').style.display='none';
+//    document.querySelector('.preloaderheader').style.display='none';
+//    },100000);
+//}
+//
+//
--- static/javascript/preloader.js
+++ static/javascript/preloader.js
@@ -1,35 +1,35 @@
-if (document.readyState == 'loading') {
-	document.addEventListener('DOMContentLoaded', ready)
-} else {
-	ready()
-}
-
-function ready() {
-    document.getElementById("searchpreloaderid").innerHTML =
-    `<div class="row container">
-        <div class="col s12 m3 l2"></div>
-        <div class="col s12 m6 l8">
-            <h6 class="center container preloaderheader">Please wait while we gather your results</h6>
-            <h6 class="center container preloaderrightsnotice">
-                All rights of the product belong to the redirected seller.
-            </h6>
-            <div class="prodiv">
-                <div class="proind"></div>
-            </div>
-        </div>
-        <div class="col s12 m3 l2"></div>
-    </div>`;
-
-	document.querySelector('.mainsearchcontent').style.display='none';
-    document.querySelector('.promos').style.display='block';
-    document.querySelector('.preloaderheader').style.display='block';
-    document.querySelector('.preloaderrightsnotice').style.display='block';
-    document.querySelector('.prodiv').classList.add('progress');
-    document.querySelector('.proind').classList.add('indeterminate');
-    setTimeout(function(){ document.querySelector('.prodiv').classList.remove('progress');
-    document.querySelector('.proind').classList.remove('indeterminate');
-    document.querySelector('.mainsearchcontent').style.display='block';
-    document.querySelector('.promos').style.display='none';
-    document.querySelector('.preloaderrightsnotice').style.display='none';
-    document.querySelector('.preloaderheader').style.display='none'; },10000);
-}
+//if (document.readyState == 'loading') {
+//	document.addEventListener('DOMContentLoaded', ready)
+//} else {
+//	ready()
+//}
+//
+//function ready() {
+//    document.getElementById("searchpreloaderid").innerHTML =
+//    `<div class="row container">
+//        <div class="col s12 m3 l2"></div>
+//        <div class="col s12 m6 l8">
+//            <h6 class="center container preloaderheader">Please wait while we gather your results</h6>
+//            <h6 class="center container preloaderrightsnotice">
+//                All rights of the product belong to the redirected seller.
+//            </h6>
+//            <div class="prodiv">
+//                <div class="proind"></div>
+//            </div>
+//        </div>
+//        <div class="col s12 m3 l2"></div>
+//    </div>`;
+//
+//	document.querySelector('.mainsearchcontent').style.display='none';
+//    document.querySelector('.promos').style.display='block';
+//    document.querySelector('.preloaderheader').style.display='block';
+//    document.querySelector('.preloaderrightsnotice').style.display='block';
+//    document.querySelector('.prodiv').classList.add('progress');
+//    document.querySelector('.proind').classList.add('indeterminate');
+//    setTimeout(function(){ document.querySelector('.prodiv').classList.remove('progress');
+//    document.querySelector('.proind').classList.remove('indeterminate');
+//    document.querySelector('.mainsearchcontent').style.display='block';
+//    document.querySelector('.promos').style.display='none';
+//    document.querySelector('.preloaderrightsnotice').style.display='none';
+//    document.querySelector('.preloaderheader').style.display='none'; },10000);
+//}
